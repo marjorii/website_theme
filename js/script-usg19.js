b401099a0301b0b50f597f5c32a4c318ce2703f2
marjorii <hello@marjorieober.com>
2019-04-12 17:51:19 +0200

    Add page for usg19

---
+++ js/script-usg19.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.item').forEach(function(elem) {
+    elem.addEventListener('click', function(event) {
+        event.preventDefault();
+        fetch(this.href + ".json")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            document.querySelector('article.project').innerHTML = json.content;
+            document.querySelector('main').classList.add('show-project');
+            window.location.hash = 'img' ;
+            document.querySelector('footer p').innerHTML = json.title + '.';
+        });
+    });
+});
+
+document.querySelectorAll('#list li > span').forEach(function(elem) {
+    elem.onmousemove = function(e) {
+        var x = (e.pageX - 100) + 'px',
+            y = (e.pageY - 100) + 'px';
+        elem.querySelector('img').style.top = y;
+        elem.querySelector('img').style.left = x;
+    }
+});
+
+document.querySelector('#cross').addEventListener('click', function(event) {
+    document.querySelector('main').classList.remove('show-project');
+});
