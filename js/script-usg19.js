7828af55baa3d19fecca6096218270e64b2fb8e4
marjorii <hello@marjorieober.com>
2019-04-16 23:49:16 +0200

    Add full screen view on postcards (postcard page)

--- js/script-usg19.js
+++ js/script-usg19.js
@@ -26,3 +26,21 @@ document.querySelectorAll('#list li > span').forEach(function(elem) {
 document.querySelector('#cross').addEventListener('click', function(event) {
     document.querySelector('main').classList.remove('show-project');
 });
+
+document.querySelectorAll('.cell').forEach(function(elem) {
+    elem.addEventListener('click', function(event) {
+        if (elem.classList.contains('full-screen')) {
+            elem.classList.remove('full-screen');
+            document.querySelector('aside.right').style.cssText = 'right: 20px;';
+            document.querySelector('header').style.cssText = 'right: 20px;';
+            document.querySelector('footer').style.cssText = 'right: 20px;';
+            document.querySelector('#cross').style.cssText = 'display: flex;';
+        } else {
+        elem.setAttribute('class', 'full-screen');
+        document.querySelector('aside.right').style.cssText = 'right: 0;';
+        document.querySelector('header').style.cssText = 'right: 0;';
+        document.querySelector('footer').style.cssText = 'right: 0;';
+        document.querySelector('#cross').style.cssText = 'display: none;';
+        }
+    });
+});
