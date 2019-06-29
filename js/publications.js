8a498216d4be1905b02fff0967b60ac7d1b25963
marjorii <hello@marjorieober.com>
2019-06-29 22:50:25 +0200

    Set main style for pages (menu-bar), add and style content in posts page, adjust blueprint for posts

---
+++ js/publications.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('.item').forEach(function(elem) {
+    elem.addEventListener('click', function(event) {
+        event.preventDefault();
+        event.target.parentElement.lastElementChild.classList.toggle('hide');
+    });
+});
+document.querySelectorAll('aside.left a').forEach(function(elem) {
+    elem.onclick = function(e) {
+        var projects = document.querySelectorAll('.project-list');
+        let other = document.querySelector('aside.left a.selected');
+        if (other) {
+            other.classList.remove('selected');
+            if (other === this) {
+                projects.forEach(function(proj) {
+                    proj.classList.remove('match');
+                });
+                return;
+            }
+        }
+        this.classList.add('selected');
+        projects.forEach(function(project) {
+            if (project.dataset.type.includes(elem.dataset.type)) {
+                project.classList.add('match');
+            }
+            else {
+                project.classList.remove('match');
+            }
+        });
+    }
+});
---
+++ js/script-blog.js
@@ -0,0 +1,37 @@
+var currentWidth = document.querySelector('header nav li:nth-of-type(2)').offsetWidth;
+document.querySelectorAll('#select-page li').forEach(function(elem) {
+    elem.style.cssText = 'width: ' + currentWidth + 'px';
+});
+document.querySelector('header nav li:nth-of-type(2) a').onclick = function(e) {
+    e.preventDefault();
+    document.getElementById('select-page').classList.toggle('hide');
+}
+document.querySelector('header nav li:nth-of-type(3) a').onmouseenter = function() {
+    document.getElementById('dancing-banana').classList.remove('hide');
+}
+document.querySelector('header nav li:nth-of-type(3) a').onmouseleave = function() {
+    document.getElementById('dancing-banana').classList.add('hide');
+}
+
+
+var isTactile = false;
+if ( /Android|webOS|iPhone|iPad|Kindle|Tablet|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    isTactile = true;
+    document.getElementById('dancing-banana').style.cssText = 'display: none';
+    document.querySelector('header').style.cssText = 'right: 0';
+    document.querySelector('footer.flex-line').style.cssText = 'right: 0';
+    document.querySelector('aside.right').style.cssText = 'right: 0';
+}
+else {
+    isTactile = false;
+}
+
+var isPhone = false;
+if ( /Android|iPhone|BlackBerry/i.test(navigator.userAgent) && window.innerWidth > window.innerHeight) {
+    isPhone = true;
+    document.querySelector('aside.left ul').style.cssText = 'display: none';
+    document.querySelector('aside.right > ul').style.cssText = 'margin-left: 0';
+}
+else {
+    isPhone = false;
+}
