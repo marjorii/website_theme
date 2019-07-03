58e7b5908e79d9de0ffce89a98008afc135ea113
marjorii <hello@marjorieober.com>
2019-07-03 17:53:56 +0200

    Style posts and texts page, and their content page. Start to style other page, adapt some js

--- js/text-blog.js
+++ js/text-blog.js
@@ -1,1 +1,8 @@
-
+var category = document.querySelector('.data').dataset.type;
+var categories = document.querySelectorAll('aside.left a');
+console.log(category);
+categories.forEach(function(cat) {
+    if (!category.includes(cat.dataset.type)) {
+        cat.classList.add('hide');
+    }
+});
