93b6706b91fdb99f488945076d2b45efed2917da
marjorii <hello@marjorieober.com>
2020-03-21 13:01:28 +0100

    Handle aside left (data-type) for individual post page in blog

---
+++ js/blog-post.js
@@ -0,0 +1,7 @@
+var category = document.querySelector('li > span').dataset.type;
+var categories = document.querySelectorAll('aside.left a');
+    categories.forEach(function(cat) {
+        if (!category.includes(cat.dataset.type)) {
+            cat.classList.add('hide');
+        }
+});
