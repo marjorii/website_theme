bb95bed6044e3f2c9093cd93fe9520f6ee5a26a5
marjorii <hello@marjorieober.com>
2020-10-12 19:01:45 +0200

    Add nav button for pages and articles, style and handle responsive

--- js/blog-posts.js
+++ js/blog-posts.js
@@ -1,3 +1,45 @@
+document.querySelector('#pages-menu input').onclick = function() {
+    let ariaExpanded = this.getAttribute('aria-expanded');
+    document.getElementById('pages').classList.toggle('hidden');
+    if ( ariaExpanded == 'false') {
+        this.setAttribute('aria-expanded', 'true');
+    }
+    else {
+        this.setAttribute('aria-expanded', 'false');
+    }
+}
+document.querySelectorAll('.page').forEach(function(page) {
+    page.firstElementChild.onclick = function(e) {
+        const currentUl = document.querySelector('.articles:not(.hidden)');
+        if (currentUl) {
+            currentUl.classList.toggle('hidden');
+            currentUl.previousElementSibling.setAttribute('aria-expanded', 'false');
+            const nextPage = currentUl.parentElement.nextElementSibling;
+            if (nextPage) {
+                nextPage.firstElementChild.classList.remove('stroke');
+            }
+            if (currentUl.previousElementSibling == this) {
+                return;
+            }
+        }
+        let ariaExpanded = this.getAttribute('aria-expanded');
+        page.lastElementChild.classList.remove('hidden');
+        this.setAttribute('aria-expanded', 'true');
+        if (page.nextElementSibling) {
+            page.nextElementSibling.firstElementChild.classList.add('stroke');
+        }
+    }
+    window.addEventListener('keyup', function(event) {
+        if (event.keyCode == 27) {
+            if (!page.lastElementChild.classList.contains('hidden')) {
+                page.lastElementChild.classList.add('hidden');
+                page.firstElementChild.setAttribute('aria-expanded', 'false');
+                page.nextElementSibling.firstElementChild.classList.remove('stroke');
+            }
+        }
+    });
+});
+
 document.querySelectorAll('.item').forEach(function(elem) {
     elem.addEventListener('auxclick', function(event) {
         event.preventDefault();
